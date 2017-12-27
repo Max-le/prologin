d0b29bc02df145d5919025c56f3f8fc1a9ebdb69
Max-le <max.09@outlook.com>
2017-12-27 19:23:49 +0100

    seems to work.

--- beach.py
+++ beach.py
@@ -1,41 +1,36 @@
 """Entrée"""
-M1 = 0
-M2 = 0
-N  = 0
+M1 = 50
+M2 = 250
+N  = 500
 
 """ position d'un client """
 x = 0
 
-
-input(M1)
-input(M2)
 """1. Trouver point ou x est indifférent d'aller chez M1 ou M2."""
 """-> Point ou dist(x,M1) = dist(x,M2)
 """
 
-"""Returne distance entre 2 points"""
-def dist(a, b):
-	d = 0
-	d = abs(a - b)
-	return d
 
 
 """ x ( client ) se démarre de N = 0."""
-while dist(x,M1) != dist(x,M2):
+while abs(M1-x) != abs(M2-x):
 
 	"""le client avance"""
 	x = x + 1
+	
 
 """ Ici le client est aussi proche de M1 que M2. (equi)"""
 equi = x
+print("equi= " , equi)
 
 """ Qui a la plus grosse ? (influence)"""
-if equi > (N/2):
+print(N/2)
+if equi > N/2:
 	winner = 1
 
-if equi < (N/2):
+elif equi < N/2:
 	winner = 2
-else:
+else :
         winner = 0
-
+        
 print(winner)
