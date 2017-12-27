d8552e6cddb95b919b630851e2e3dfdd4fc8ba4f
Max-le <max.09@outlook.com>
2017-12-27 18:55:50 +0100

    Testing distance calculation

--- beach.py
+++ beach.py
@@ -1,1 +1,16 @@
-beach.py
+"""Entrée"""
+M1 = 0
+M2 = 0
+N  = 0
+
+""" position d'un client """
+x = 0
+
+"""1. Trouver point ou x est indifférent d'aller chez M1 ou M2."""
+"""-> Point ou dist(x,M1) = dist(x,M2)
+"""
+
+"""Returne distance entre 2 points"""
+def dist(a, b):
+	d = abs(a - b)
+	return d
