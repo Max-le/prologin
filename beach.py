4245d5b7a0ba5481d0e92e8a58ed8e7efe7f66f5
Max-le <max.09@outlook.com>
2017-12-27 19:05:19 +0100

    work done, no tested yet.

--- beach.py
+++ beach.py
@@ -16,3 +16,21 @@ def dist(a, b):
 	d = abs(a - b)
 	return d
 
+
+""" x ( client ) se démarre de N = 0."""
+while dist(x,M1) != dist(x,M2):
+
+	"""le client avance"""
+	x = x + 1
+
+""" Ici le client est aussi proche de M1 que M2. (equi)"""
+equi = x
+
+""" Qui a la plus grosse ? (influence)"""
+if equi > (N/2):
+	winner = 1
+
+if equi < (N/2):
+	winner = 2
+else:
+winner = 0
